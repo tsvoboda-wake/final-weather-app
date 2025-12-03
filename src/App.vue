91fc14c02deeb957c0eaba45268b9753bcf8084c
tsvoboda-wake <taylorsmithfromm+wake@gmail.com>
2025-12-02 23:49:30 -0500

    feat: navigate between views

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
 import { ref } from 'vue'
 import SearchInput from './components/SearchInput.vue'
-import WeatherCard from './components/WeatherCard.vue'
-
+import LocationsList from './components/LocationsList.vue'
+import ForecastView from './components/ForecastView.vue'
+import HourlyView
+ from './components/HourlyView.vue'
 const places = ref([])
 const addPlace = (data) => {
   places.value.push(data)
 }
 
+const view = ref('main')
+const detailLocation = ref(null)
+
 const deletePlace = (name) => {
-  if (confirm('Are you sure?')) {
     places.value = places.value.filter((p) => p.location.name !== name)
-  }
+}
+
+const setDetailView = (newView, place) => {
+  view.value = newView
+  detailLocation.value = place
 }
 </script>
 <template>
@@ -27,17 +35,42 @@ const deletePlace = (name) => {
         })
       }}
     </div>
+    <div v-show="view === 'main'">
+      <!-- Search -->
+      <div>
+        <!-- when place-data is received, addPlace() to push to saved locations array -->
+        <SearchInput @place-data="addPlace" />
+      </div>
 
-    <!-- Search -->
-    <div>
-      <SearchInput @place-data="addPlace" />
-    </div>
-
-    <!-- Weather cards -->
-    <div class="grid grid-cols-2 gap-4">
-      <div v-for="(place, idx) in places" :key="idx">
-        <WeatherCard :place="place" @delete-place="deletePlace" />
+      <!-- Saved Locations List -->
+      <div>
+        <div v-for="(place, idx) in places" :key="idx">
+          <LocationsList 
+            :place="place"
+            :view="view"
+            @delete-place="deletePlace"
+            @set-detail-view="setDetailView"
+          />
+        </div>
       </div>
     </div>
+    
+    <!-- Hourly View -->
+    <div v-if="view === 'hourly' && detailLocation !== null">
+        <HourlyView 
+          :place="detailLocation"
+          :view="view"
+          @back-to-search="view='main'"
+        />
+     </div>
+
+    <!-- Forecast View -->
+     <div v-if="view === 'forecast' && detailLocation !== null">
+        <ForecastView 
+          :place="detailLocation"
+          :view="view"
+          @back-to-search="view='main'"
+        />
+     </div>
   </main>
 </template>
